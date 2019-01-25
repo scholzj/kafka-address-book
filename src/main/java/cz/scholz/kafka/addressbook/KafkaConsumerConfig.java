0d622fa2414dbabc42ce1cfa756843b72ccacefb
Jakub Scholz <www@scholzj.com>
2019-01-26 00:14:23 +0100

    Add SSL and SASL support

--- src/main/java/cz/scholz/kafka/addressbook/KafkaConsumerConfig.java
+++ src/main/java/cz/scholz/kafka/addressbook/KafkaConsumerConfig.java
@@ -21,6 +21,24 @@ public class KafkaConsumerConfig {
     @Value("${KAFKA_BOOTSTRAP_SERVER}")
     private String bootstrapServer;
 
+    @Value("${TRUSTSTORE_PATH:#{null}}")
+    private String truststorePath;
+
+    @Value("${TRUSTSTORE_PASSWORD:#{null}}")
+    private String truststorePassword;
+
+    @Value("${KEYSTORE_PATH:#{null}}")
+    private String keystorePath;
+
+    @Value("${KEYSTORE_PASSWORD:#{null}}")
+    private String keystorePassword;
+
+    @Value("${USERNAME:#{null}}")
+    private String username;
+
+    @Value("${PASSWORD:#{null}}")
+    private String password;
+
     @Bean
     public ConsumerFactory<String, Address> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
@@ -36,6 +54,33 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+
+        // TLS
+        if (truststorePassword != null && truststorePath != null)   {
+            props.put("security.protocol", "SSL");
+            props.put("ssl.truststore.type", "PKCS12");
+            props.put("ssl.truststore.password", truststorePassword);
+            props.put("ssl.truststore.location", truststorePath);
+        }
+
+        if (keystorePassword != null && keystorePath != null)   {
+            props.put("security.protocol", "SSL");
+            props.put("ssl.keystore.type", "PKCS12");
+            props.put("ssl.keystore.password", keystorePassword);
+            props.put("ssl.keystore.location", keystorePath);
+        }
+
+        if (username != null && password != null)   {
+            props.put("sasl.mechanism","SCRAM-SHA-512");
+            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + username + "\" password=\"" + password + "\";");
+
+            if (props.get("security.protocol") != null && props.get("security.protocol").equals("SSL"))  {
+                props.put("security.protocol","SASL_SSL");
+            } else {
+                props.put("security.protocol","SASL_PLAINTEXT");
+            }
+        }
+
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
--- src/main/java/cz/scholz/kafka/addressbook/KafkaProducerConfig.java
+++ src/main/java/cz/scholz/kafka/addressbook/KafkaProducerConfig.java
@@ -20,19 +20,64 @@ public class KafkaProducerConfig {
     @Value("${KAFKA_BOOTSTRAP_SERVER}")
     private String bootstrapServer;
 
+    @Value("${TRUSTSTORE_PATH:#{null}}")
+    private String truststorePath;
+
+    @Value("${TRUSTSTORE_PASSWORD:#{null}}")
+    private String truststorePassword;
+
+    @Value("${KEYSTORE_PATH:#{null}}")
+    private String keystorePath;
+
+    @Value("${KEYSTORE_PASSWORD:#{null}}")
+    private String keystorePassword;
+
+    @Value("${USERNAME:#{null}}")
+    private String username;
+
+    @Value("${PASSWORD:#{null}}")
+    private String password;
+
     @Bean
     public ProducerFactory<String, Address> producerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(
+        Map<String, Object> props = new HashMap<>();
+        props.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
                 bootstrapServer);
-        configProps.put(
+        props.put(
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
-        configProps.put(
+        props.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+
+        // TLS
+        if (truststorePassword != null && truststorePath != null)   {
+            props.put("security.protocol", "SSL");
+            props.put("ssl.truststore.type", "PKCS12");
+            props.put("ssl.truststore.password", truststorePassword);
+            props.put("ssl.truststore.location", truststorePath);
+        }
+
+        if (keystorePassword != null && keystorePath != null)   {
+            props.put("security.protocol", "SSL");
+            props.put("ssl.keystore.type", "PKCS12");
+            props.put("ssl.keystore.password", keystorePassword);
+            props.put("ssl.keystore.location", keystorePath);
+        }
+
+        if (username != null && password != null)   {
+            props.put("sasl.mechanism","SCRAM-SHA-512");
+            props.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + username + "\" password=\"" + password + "\";");
+
+            if (props.get("security.protocol") != null && props.get("security.protocol").equals("SSL"))  {
+                props.put("security.protocol","SASL_SSL");
+            } else {
+                props.put("security.protocol","SASL_PLAINTEXT");
+            }
+        }
+
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
